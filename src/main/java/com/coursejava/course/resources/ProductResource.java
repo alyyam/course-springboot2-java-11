9fc54c91656b0858b56e5c19ac6fbff64aa9d915
Alysson Machado <alysson-am@hotmail.com>
2022-04-20 13:31:26 +1000

    Product

---
+++ src/main/java/com/coursejava/course/resources/ProductResource.java
@@ -0,0 +1,40 @@
+package com.coursejava.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.coursejava.course.entities.Product;
+import com.coursejava.course.services.ProductService;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResource {
+
+	@Autowired
+	private ProductService service;
+
+	@GetMapping
+	public ResponseEntity<List<Product>> findAll() {
+
+		List<Product> list = service.findAll();
+
+		return ResponseEntity.ok().body(list);
+
+	}
+	
+	@GetMapping(value =  "/{id}") // indica que minha requisicao aceita um ID
+	public ResponseEntity<Product> findByID (@PathVariable Long id){
+		
+		Product obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);       
+		
+	}
+	
+
+}
