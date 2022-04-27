d22bb641ff1730573924e990b94d96502c305409
Alysson Machado <alysson-am@hotmail.com>
2022-04-27 13:55:54 +1000

    User insert

--- src/main/java/com/coursejava/course/services/UserService.java
+++ src/main/java/com/coursejava/course/services/UserService.java
@@ -27,5 +27,11 @@ public class UserService {
 		Optional<User> obj = repository.findById(Id);
 		return obj.get();
 	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	
+	
 
 }
